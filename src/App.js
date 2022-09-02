f71f7bddc5f2c983729b0c7916b8b38b8aab1b97
Jianhang.Song <boyan!23>
2022-09-02 17:56:24 +0800

    0902

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import React, { Component } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import About from './components/About'
+import Home from './components/Home'
 
 export default class App extends Component {
   render() {
-    return <div>App.....</div>
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header">
+              <h2>React Router Demo</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/* 编写路由器链接 */}
+              <Link className="list-group-item active" to="/about">
+                About
+              </Link>
+              <Link className="list-group-item " to="/home">
+                Home
+              </Link>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {/* 注册路由 */}
+                <Routes>
+                  <Route path="/about" component={About} />
+                  <Route path="/home" component={Home} />
+                </Routes>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM  from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// 全局包裹路由器
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.js'
 
-// ReactDOM.render(<App/>,document.getElementById('root'))
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+)
